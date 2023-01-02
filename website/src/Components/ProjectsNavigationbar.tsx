cb464a7c50fec6b04051ef33df1b1c8895c304c2
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2023-01-02 09:53:45 +0100

    Navbar stuff

--- website/src/Components/ProjectsNavigationbar.tsx
+++ website/src/Components/ProjectsNavigationbar.tsx
@@ -1,15 +1,17 @@
-import { Navbar, Spacer } from "@nextui-org/react";
+import { Grid, Navbar, Spacer } from "@nextui-org/react";
 
 export const ProjectsNavigationbar = () => {
     return (
         <div>
-            <Navbar isBordered isCompact shouldHideOnScroll variant={"floating"} css={{left: '44%', right: '44%', maxWidth: '12%',}}>
+            <Grid.Container justify="center">
+            <Navbar isBordered isCompact shouldHideOnScroll variant={"floating"} maxWidth="xs" css={{'@xs': { display: 'flex', maxWidth: 250,  }}}>
                 <Navbar.Content enableCursorHighlight variant={'highlight'}>
                     <Navbar.Link href="/projects/games">Games</Navbar.Link>
                     <Spacer x={1.15} />
                     <Navbar.Link href="/projects/software">Software</Navbar.Link>
                 </Navbar.Content>
             </Navbar>
+            </Grid.Container>
         </div>
     );
 };
--- website/src/Pages/About.tsx
+++ website/src/Pages/About.tsx
@@ -1,7 +1,24 @@
+import { Grid, Image, Input, Spacer, Textarea } from "@nextui-org/react";
+import { loc_de } from "../Localization/de-de";
+
 export const About = () => {
     return (
         <div>
-            
+            <Spacer y={2} />
+
+            <Grid.Container justify="flex-start">
+                <Spacer x={7} />
+
+                <Grid>
+                    <Image src="https://via.placeholder.com/300" />
+                </Grid>
+
+                <Spacer x={4} />
+
+                <Grid>
+                    <Textarea readOnly minRows={1} maxRows={30} width="1000" value={loc_de.about} />
+                </Grid>
+            </Grid.Container>
         </div>
     );
 };
